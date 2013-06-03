59fcb9c794eb946e61367496555bb62c4cfa057f
xmduhan <18906021980@189.cn>
2013-06-03 23:12:43 +0800

    完成django简单的服务端数据返回程序

--- qisite/qi/urls.py
+++ qisite/qi/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from qi import service
 
 urlpatterns = patterns('',
-    url(r'^service/test', service.test, name='test'),    
+    url(r'^service/test', service.test, name='test'),
+    url(r'^service/getCatalogPaper', service.getCatalogPaper, name='getCatalogPaper'),
 )
