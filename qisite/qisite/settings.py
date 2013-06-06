5994ba921a9fdea9a0213fcf12ea04384a8dbf4a
xmduhan <18906021980@189.cn>
2013-06-06 15:38:04 +0800

    1

--- qisite/qisite/settings.py
+++ qisite/qisite/settings.py
@@ -14,13 +14,15 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ORADOGDB110',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'DJANGO2',
-        'PASSWORD': 'DJANGO2123',
-        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
+        #'ENGINE': 'django.db.backends.oracle',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'ORADOGDB110',  # Or path to database file if using sqlite3.
+        #The following settings are not used with sqlite3:
+        #'USER': 'DJANGO2',
+        #'PASSWORD': 'DJANGO2123',
+        #'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '',  # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'D:/android/workspace/qi/qisite/sqlite3.db',
     }
 }
 
