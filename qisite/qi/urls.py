9924726bfa2ab96646a0595d61f7b87e7d74c424
xmduhan <18906021980@189.cn>
2013-07-11 11:33:52 +0800

    i

--- qisite/qi/urls.py
+++ qisite/qi/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^service/userLogin', service.userLogin, name='userLogin'),
     url(r'^service/getCurrentUser', service.getCurrentUser, name='getCurrentUser'),
     url(r'^service/userRegister', service.userRegister, name='userRegister'),
-    url(r'^service/getUserPaper', service.getUserPaper, name='getUserPaper'), 
+    url(r'^service/getUserPaper', service.getUserPaper, name='getUserPaper'),  
     
 ) 
